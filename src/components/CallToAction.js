95dc9841f8102a3b63641e76e139f0f48931717c
Sunand Pallapati <psunand16@gmail.com>
2023-09-25 22:26:30 -0400

    Reserve Table section styling changes

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -5,13 +5,15 @@ const CallToAction =()=>{
     return(
         <div className="Section">
             <div>
-                <h1>Little Lemon</h1>
-                <h3>Chicago</h3>
-                <h4> We are a family owned Mediterranean restaurant focussed on traditional recipes served with a moderrn twist. </h4>
+                <h1 id="logo">Little Lemon</h1>
+                <h3 id="subHeading">Chicago</h3>
+                <p id="logoDesc"> We are a family owned <br/>Mediterranean restaurant<br/> focussed on traditional <br/>recipes served with a moderrn <br/>twist. </p>
             </div>
             <div>
-                <button>Reserve a Table</button>
-                <img src="../images/DisplayImage.jpeg" id="displayImage" alt="Display Image"></img>
+                <button type="button" id ="tableButton" class="btn btn-primary">Reserve a Table</button>
+            </div>
+            <div>
+                <img src="../images/DisplayImage.jpeg" id="DisplayImage" alt="Display Image"></img>
             </div>
         </div>
     )
