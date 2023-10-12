36aa7ca8c30e5fcfc0e10c66c8b4d434122e0609
Sunand Pallapati <psunand16@gmail.com>
2023-10-11 20:01:38 -0400

    Testimonals section changes

--- src/components/CustomersSay.js
+++ src/components/CustomersSay.js
@@ -3,7 +3,52 @@
 
 const CustomersSay = () => {
     return (
-        <h2 className="TestimonalsSection">This is Testimonals Section</h2>
+        <>
+         <div className="TestimonalsSection">
+              <div>
+                    <h2>Testimonals</h2>
+              </div>
+              <div className="testimonalCardArea">
+                <div className="testimonalContentArea">
+                <div className="rating">
+                    <div className="ratingContext">
+                        <h3>Rating</h3>
+                        <img src=""></img>
+                        <h5>John</h5>
+                        <h6>Review text</h6>
+                    </div>
+                </div>
+                <div className="rating">
+                    <div className="ratingContext">
+                        <h3>Rating</h3>
+                        <img src=""></img>
+                        <h5>Anna</h5>
+                        <h6>Review text</h6>
+                    </div>
+                </div>
+                <div className="rating">
+                    <div className="ratingContext">
+                        <h3>Rating</h3>
+                        <img src=""></img>
+                        <h5>Dimitri</h5>
+                        <h6>Review text</h6>
+                    </div>
+                </div>
+                <div className="rating">
+                    <div className="ratingContext">
+                        <h3>Rating</h3>
+                        <img src=""></img>
+                        <h5>Jake</h5>
+                        <h6>Review text</h6>
+                    </div>
+                </div>
+                </div>
+              </div>
+
+        </div>
+
+        </>
+
     )
 }
 
