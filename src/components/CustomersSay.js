8558f9c61d15a4f1bbd1d91a947cb4a3959ef478
Sunand Pallapati <psunand16@gmail.com>
2023-10-13 10:53:24 -0400

    Testimonals Section updates

--- src/components/CustomersSay.js
+++ src/components/CustomersSay.js
@@ -9,41 +9,94 @@ const CustomersSay = () => {
                     <h2>Testimonals</h2>
                 </div>
                 <div className="testimonalArea">
-                    <div className="reviewArea">
-                        <div className="ratingArea">
-                            <h3>Rating</h3>
-                            <div id='ratingStar'>
-                                <img src="../images/star.jpeg"></img>
-                                <img src="../images/star.jpeg"></img>
-                                <img src="../images/star.jpeg"></img>
-                                <img src="../images/star.jpeg"></img>
-                                <img src="../images/star.jpeg"></img>
+                    <div className="reviewSection">
+                        <div className="reviewArea">
+                            <div className="ratingArea">
+                                <h3>Rating</h3>
+                                <div id='ratingStar'>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                </div>
                             </div>
+                            <div className="profileArea">
+                                <img src="../images/john.jpeg" id="profile"></img>
+                                <h5>John</h5>
+                            </div>
+                            <div>
+                                <h6>Review text</h6>
+                            </div>
+
                         </div>
-                        <div className="profileArea">
-                            <img src="../images/john.jpeg" id="profile"></img>
-                            <h5>John</h5>
-                        </div>
-                        <h6>Review text</h6>
+
                     </div>
 
-
-                    <div className="reviewArea">
-                        <div className="ratingArea">
-                            <h3>Rating</h3>
-                            <div id='ratingStar'>
-                                <img src="../images/star.jpeg"></img>
-                                <img src="../images/star.jpeg"></img>
-                                <img src="../images/star.jpeg"></img>
-                                <img src="../images/star.jpeg"></img>
-                                <img src="../images/star.jpeg"></img>
+                    <div className="reviewSection">
+                        <div className="reviewArea">
+                            <div className="ratingArea">
+                                <h3>Rating</h3>
+                                <div id='ratingStar'>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                </div>
+                            </div>
+                            <div className="profileArea">
+                                <img src="../images/anna.jpeg" id="profile"></img>
+                                <h5>Anna</h5>
+                            </div>
+                            <div>
+                                <h6>Review text</h6>
                             </div>
                         </div>
-                        <div className="profileArea">
-                            <img src="../images/john.jpeg" id="profile"></img>
-                            <h5>John</h5>
+                    </div>
+
+                    <div className="reviewSection">
+                        <div className="reviewArea">
+                            <div className="ratingArea">
+                                <h3>Rating</h3>
+                                <div id='ratingStar'>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                </div>
+                            </div>
+                            <div className="profileArea">
+                                <img src="../images/dimitri.jpeg" id="profile"></img>
+                                <h5>Dimitri</h5>
+                            </div>
+                            <div>
+                                <h6>Review text</h6>
+                            </div>
                         </div>
-                        <h6>Review text</h6>
+                    </div>
+
+                    <div className="reviewSection">
+                        <div className="reviewArea">
+                            <div className="ratingArea">
+                                <h3>Rating</h3>
+                                <div id='ratingStar'>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                    <img src="../images/star.jpeg"></img>
+                                </div>
+                            </div>
+                            <div className="profileArea">
+                                <img src="../images/jake.jpeg" id="profile"></img>
+                                <h5>Jake</h5>
+                            </div>
+                            <div>
+                                <h6>Review text</h6>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
