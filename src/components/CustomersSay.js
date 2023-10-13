ab5fb4fba0f7ce3f0e4a99cf738f97b038984840
Sunand Pallapati <psunand16@gmail.com>
2023-10-12 20:45:29 -0400

    Testimonal section updates

--- src/components/CustomersSay.js
+++ src/components/CustomersSay.js
@@ -4,48 +4,51 @@
 const CustomersSay = () => {
     return (
         <>
-         <div className="TestimonalsSection">
-              <div>
+            <div className="TestimonalsSection">
+                <div>
                     <h2>Testimonals</h2>
-              </div>
-              <div className="testimonalCardArea">
-                <div className="testimonalContentArea">
-                <div className="rating">
-                    <div className="ratingContext">
-                        <h3>Rating</h3>
-                        <img src=""></img>
-                        <h5>John</h5>
+                </div>
+                <div className="testimonalArea">
+                    <div className="reviewArea">
+                        <div className="ratingArea">
+                            <h3>Rating</h3>
+                            <div id='ratingStar'>
+                                <img src="../images/star.jpeg"></img>
+                                <img src="../images/star.jpeg"></img>
+                                <img src="../images/star.jpeg"></img>
+                                <img src="../images/star.jpeg"></img>
+                                <img src="../images/star.jpeg"></img>
+                            </div>
+                        </div>
+                        <div className="profileArea">
+                            <img src="../images/john.jpeg" id="profile"></img>
+                            <h5>John</h5>
+                        </div>
                         <h6>Review text</h6>
                     </div>
-                </div>
-                <div className="rating">
-                    <div className="ratingContext">
-                        <h3>Rating</h3>
-                        <img src=""></img>
-                        <h5>Anna</h5>
-                        <h6>Review text</h6>
-                    </div>
-                </div>
-                <div className="rating">
-                    <div className="ratingContext">
-                        <h3>Rating</h3>
-                        <img src=""></img>
-                        <h5>Dimitri</h5>
-                        <h6>Review text</h6>
-                    </div>
-                </div>
-                <div className="rating">
-                    <div className="ratingContext">
-                        <h3>Rating</h3>
-                        <img src=""></img>
-                        <h5>Jake</h5>
-                        <h6>Review text</h6>
-                    </div>
-                </div>
-                </div>
-              </div>
 
-        </div>
+
+                    <div className="reviewArea">
+                        <div className="ratingArea">
+                            <h3>Rating</h3>
+                            <div id='ratingStar'>
+                                <img src="../images/star.jpeg"></img>
+                                <img src="../images/star.jpeg"></img>
+                                <img src="../images/star.jpeg"></img>
+                                <img src="../images/star.jpeg"></img>
+                                <img src="../images/star.jpeg"></img>
+                            </div>
+                        </div>
+                        <div className="profileArea">
+                            <img src="../images/john.jpeg" id="profile"></img>
+                            <h5>John</h5>
+                        </div>
+                        <h6>Review text</h6>
+                    </div>
+                </div>
+
+            </div>
+
 
         </>
 
