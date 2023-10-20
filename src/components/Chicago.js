264ad8223bce826746485b744264d4fb805f9663
Sunand Pallapati <psunand16@gmail.com>
2023-10-20 15:45:52 -0400

    Chicago section updates and responsive changes

--- src/components/Chicago.js
+++ src/components/Chicago.js
@@ -5,30 +5,36 @@ const Chicago = () => {
     return (
         <>
             <div className="AboutSection">
-                <div className="aboutContent">
-                    <div className="aboutLogoArea">
-                        <div id="aboutHeadings">
-                            <h1>Little Lemon</h1>
-                            <h3 id="aboutSubHeading">Chicago</h3>
+                <div className="aboutSectionArea">
+
+                    <div className="aboutContent">
+                        <div className="aboutLogoArea">
+                            <div id="aboutHeadings">
+                                <h1>Little Lemon</h1>
+                                <h3 id="aboutSubHeading">Chicago</h3>
+                            </div>
+                            <div id="aboutContext">
+                                <p id="aboutLogoDesc"> Amet minim mollit non deserunt <br /> ullamco est sit aliqua dolor do amet<br /> sint. Velit officia consequat duis enim <br /> velit mollit. Exercitation veniam <br /> consequat sunt nostrud amet.<br />
+                                    Amet minim mollit non deserunt<br /> ullamco est sit aliqua dolor do amet <br />sint. Velit officia consequat duis enim<br /> velit mollit. </p>
+                            </div>
+
                         </div>
-                        <div id="aboutContext">
-                            <p id="aboutLogoDesc"> Amet minim mollit non deserunt <br /> ullamco est sit aliqua dolor do amet<br /> sint. Velit officia consequat duis enim <br /> velit mollit. Exercitation veniam <br /> consequat sunt nostrud amet.<br />
-                                Amet minim mollit non deserunt<br /> ullamco est sit aliqua dolor do amet <br />sint. Velit officia consequat duis enim<br /> velit mollit. </p>
+                    </div>
+                    <div className="aboutImageContent">
+                        <div className="aboutImageArea">
+                            <div className="aboutDisplayImages">
+                                <img src="../images/chicago2.jpeg" id="aboutImage1"></img>
+                                <img src="../images/chicago1.jpeg" id="aboutImage2"></img>
+                            </div>
+
                         </div>
 
                     </div>
+
+
+
                 </div>
-                <div className="imageContent">
-                <div className="aboutImageArea">
-                    <div className="aboutDisplayImage">
-                        <img src="../images/chicago2.jpeg" id="aboutDisplayImage1" alt="Display"></img>
-                        <img src="../images/chicago1.jpeg" id="aboutDisplayImage2" alt="Display"></img>
-                    </div>
-                     {/* <div className="aboutDisplayImage2">
-                        <img src="../images/chicago1.jpeg" id="aboutDisplayImage2" alt="Display"></img>
-                    </div>  */}
-                </div>
-                </div>
+
             </div>
         </>
     )
